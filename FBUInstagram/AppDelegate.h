495bd80276e5daeb032b58712aed3c8135eefbb8
mary-jiang <mary.jiang2020@gmail.com>
2021-07-06 15:33:21 -0400

    Initial Commit

---
+++ FBUInstagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FBUInstagram
+//
+//  Created by Mary Jiang on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FBUInstagram/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FBUInstagram
+//
+//  Created by Mary Jiang on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
